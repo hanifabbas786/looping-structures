97854d82b6ae4626dcc6731edd728ecf8b4195cf
dev_hanif <152361547+hanifabbas786@users.noreply.github.com>
2024-03-24 22:03:03 +0500

    Initial commit

---
+++ back couning from 10-----1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+a=10;
+do{
+	cout<<a<<endl;
+	a=a-1;
+	
+}
+while(a>=1);
+	return 0;
+}
+
---
+++ program display five number and sum using while loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,sum;
+	sum=0;
+	a=1;
+	while(a<=5)
+	{cout<<a<<endl;
+	sum=sum+a;
+	a=a+1;
+	}
+cout<<"sum is"<<sum;
+	return 0;
+}
+
